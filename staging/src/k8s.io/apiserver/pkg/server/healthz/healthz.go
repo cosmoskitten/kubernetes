6531bfd543fac4f64d954d86a22d21a33208f8bd
Morgan Bauer <mbauer@us.ibm.com>
2017-04-10 20:29:40 -0700

    basic logging for healthz installer
     - InstallHandler is the public interface through which all interaction
       occurs.
     - It is good to know whether the default ping is occurring to know due
       to manual installation or automatic installation.
     - It is good to know how many handlers are installed to see whether
       code changes are taking effect.
     - It is good to know the names of the handlers that are installed to
       make sure that a handler a user thinks is installed is being
       installed at runtime.

--- staging/src/k8s.io/apiserver/pkg/server/healthz/healthz.go
+++ staging/src/k8s.io/apiserver/pkg/server/healthz/healthz.go
@@ -21,6 +21,8 @@ import (
 	"fmt"
 	"net/http"
 	"sync"
+
+	"github.com/golang/glog"
 )
 
 // HealthzChecker is a named healthz checker.
@@ -58,13 +60,19 @@ func NamedCheck(name string, check func(r *http.Request) error) HealthzChecker {
 	return &healthzCheck{name, check}
 }
 
-// InstallHandler registers a handler for health checking on the path "/healthz" to mux.
+// InstallHandler registers handlers for health checking on the path
+// "/healthz" to mux. *All handlers* for mux must be specified in
+// exactly one call to InstallHandler. Calling InstallHandler more
+// than once for the same mux will result in a panic.
 func InstallHandler(mux mux, checks ...HealthzChecker) {
 	if len(checks) == 0 {
+		glog.Info("No default health checks specified. Installing the ping handler.")
 		checks = []HealthzChecker{PingHealthz}
 	}
+	glog.Infof("Installing %v healthz checkers", len(checks))
 	mux.Handle("/healthz", handleRootHealthz(checks...))
 	for _, check := range checks {
+		glog.Infof("Installing %q healthz checker", check.Name())
 		mux.Handle(fmt.Sprintf("/healthz/%v", check.Name()), adaptCheckToHandler(check.Check))
 	}
 }
