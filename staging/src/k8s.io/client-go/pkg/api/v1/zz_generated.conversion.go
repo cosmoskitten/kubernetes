2792ad8e4d190e231715f3aa1e8e8bd50a5b0c95
Klaus Ma <madaxa@cn.ibm.com>
2017-06-08 16:07:20 +0800

    generated client-go.

--- staging/src/k8s.io/client-go/pkg/api/v1/zz_generated.conversion.go
+++ staging/src/k8s.io/client-go/pkg/api/v1/zz_generated.conversion.go
@@ -3192,6 +3192,7 @@ func autoConvert_v1_PodAffinityTerm_To_api_PodAffinityTerm(in *PodAffinityTerm, 
 	out.LabelSelector = (*meta_v1.LabelSelector)(unsafe.Pointer(in.LabelSelector))
 	out.Namespaces = *(*[]string)(unsafe.Pointer(&in.Namespaces))
 	out.TopologyKey = in.TopologyKey
+	out.NumOfMatchingPods = in.NumOfMatchingPods
 	return nil
 }
 
@@ -3204,6 +3205,7 @@ func autoConvert_api_PodAffinityTerm_To_v1_PodAffinityTerm(in *api.PodAffinityTe
 	out.LabelSelector = (*meta_v1.LabelSelector)(unsafe.Pointer(in.LabelSelector))
 	out.Namespaces = *(*[]string)(unsafe.Pointer(&in.Namespaces))
 	out.TopologyKey = in.TopologyKey
+	out.NumOfMatchingPods = in.NumOfMatchingPods
 	return nil
 }
 
