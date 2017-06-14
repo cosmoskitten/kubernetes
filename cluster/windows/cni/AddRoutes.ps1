3d51819bdca25fda405f54391cc48455301999f9
Madhan Raj Mookkandy <madhanm@microsoft.com>
2017-06-14 16:27:39 -0700

    Following are part of this commit +++++++++++++++++++++++++++++++++ * Windows CNI Support 	(1) Support to use --network-plugin=cni 	(2) Handled platform requirement of calling CNI ADD for all the containers. 	(2.1) For POD Infra container, netNs has to be empty 	(2.2) For all other containers, sharing the network namespace of POD container, 	      should pass netNS name as "container:<Pod Infra Container Id>", same as the               NetworkMode of the current container 	(2.3) The Windows CNI plugin has to handle this to call into Platform.               Sample Windows CNI Plugin code to be shared soon. * Sandbox support for Windows 	(1) Sandbox support for Windows. Works only with Docker runtime. 	(2) Retained CONTAINER_NETWORK as a backward compatibilty flag, 	    to not break existing deployments using it. 	(3) Works only with CNI plugin enabled.
    (*) Sample Script to start Kubelet and KubeProxy
    (*) Changes to reinvoke CNI ADD for every new container created. This is hooked up with PodStatus,
        but would be ideal to move it outside of this, once we have CNI GET support

---
+++ cluster/windows/cni/AddRoutes.ps1
@@ -0,0 +1,38 @@
+﻿$endpointName = "cbr0"
+$vnicName = "vEthernet ($endpointName)"
+
+function
+Add-RouteToPodCIDR()
+{
+    '''
+    $route = get-netroute -InterfaceAlias "$vnicName" -DestinationPrefix $clusterCIDR -erroraction Ignore
+    if (!$route) 
+    {
+        New-Netroute -DestinationPrefix $clusterCIDR -InterfaceAlias "$vnicName" -NextHop 0.0.0.0 -Verbose
+    }
+
+    '''
+    $podCIDRs=c:\k\kubectl.exe  --kubeconfig=c:\k\config get nodes -o=custom-columns=Name:.status.nodeInfo.operatingSystem,PODCidr:.spec.podCIDR --no-headers
+    Write-Host "Add-RouteToPodCIDR - available nodes $podCIDRs"
+    foreach ($podcidr in $podCIDRs)
+    {
+        $tmp = $podcidr.Split(" ")
+        $os = $tmp | select -First 1
+        $cidr = $tmp | select -Last 1
+        $cidrGw =  $cidr.substring(0,$cidr.lastIndexOf(".")) + ".1"
+
+        
+        if ($os -eq "windows") {
+            $cidrGw = $cidr.substring(0,$cidr.lastIndexOf(".")) + ".2"
+        }
+
+        Write-Host "Adding route for Remote Pod CIDR $cidr, GW $cidrGw, for node type $os"
+
+        $route = get-netroute -InterfaceAlias "$vnicName" -DestinationPrefix $cidr -erroraction Ignore
+        if (!$route) {
+            new-netroute -InterfaceAlias "$vnicName" -DestinationPrefix $cidr -NextHop  $cidrGw -Verbose
+        }
+    }
+}
+
+Add-RouteToPodCIDR
