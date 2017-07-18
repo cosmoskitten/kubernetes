146eff34c914713867462cb8ae4d393985d3971b
allencloud <allen.sun@daocloud.io>
2017-07-18 14:12:57 +0800

    validate kube-proxy options
    Signed-off-by: allencloud <allen.sun@daocloud.io>

---
+++ pkg/apis/componentconfig/validation/validation.go
@@ -0,0 +1,109 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package validation
+
+import (
+	"net"
+
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	"k8s.io/kubernetes/pkg/apis/componentconfig"
+)
+
+// ValidateKubeProxyConfiguration validates the configuration of kube-proxy
+func ValidateKubeProxyConfiguration(config *componentconfig.KubeProxyConfiguration) field.ErrorList {
+	allErrs := field.ErrorList{}
+
+	newPath := field.NewPath("KubeProxyConfiguration")
+
+	allErrs = append(allErrs, validateKubeProxyIPTablesConfiguration(config.IPTables, newPath.Child("KubeProxyIPTablesConfiguration"))...)
+	allErrs = append(allErrs, validateKubeProxyConntrackConfiguration(config.Conntrack, newPath.Child("KubeProxyConntrackConfiguration"))...)
+	allErrs = append(allErrs, validateProxyMode(config.Mode, newPath.Child("Mode"))...)
+
+	if config.OOMScoreAdj != nil && (*config.OOMScoreAdj < -1000 || *config.OOMScoreAdj > 1000) {
+		allErrs = append(allErrs, field.Invalid(newPath.Child("OOMScoreAdj"), *config.OOMScoreAdj, "must be within the range [-1000, 1000]"))
+	}
+
+	if config.UDPIdleTimeout.Duration <= 0 {
+		allErrs = append(allErrs, field.Invalid(newPath.Child("UDPIdleTimeout"), config.UDPIdleTimeout, "must be greater than 0"))
+	}
+
+	if config.ConfigSyncPeriod.Duration <= 0 {
+		allErrs = append(allErrs, field.Invalid(newPath.Child("ConfigSyncPeriod"), config.ConfigSyncPeriod, "must be greater than 0"))
+	}
+
+	if ip := net.ParseIP(config.BindAddress); ip == nil {
+		allErrs = append(allErrs, field.Invalid(newPath.Child("BindAddress"), config.BindAddress, "not a valid textual representation of an IP address"))
+	}
+
+	if _, _, err := net.SplitHostPort(config.HealthzBindAddress); err != nil {
+		allErrs = append(allErrs, field.Invalid(newPath.Child("HealthzBindAddress"), config.HealthzBindAddress, err.Error()))
+	}
+
+	return allErrs
+}
+
+func validateKubeProxyIPTablesConfiguration(config componentconfig.KubeProxyIPTablesConfiguration, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+
+	if config.MasqueradeBit != nil && (*config.MasqueradeBit < 0 || *config.MasqueradeBit > 31) {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("MasqueradeBit"), config.MasqueradeBit, "must be within the range [0, 31]"))
+	}
+
+	if config.SyncPeriod.Duration <= 0 {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("SyncPeriod"), config.SyncPeriod, "must be greater than 0"))
+	}
+
+	if config.MinSyncPeriod.Duration < 0 {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("MinSyncPeriod"), config.MinSyncPeriod, "cannot be less than 0"))
+	}
+
+	return allErrs
+}
+
+func validateKubeProxyConntrackConfiguration(config componentconfig.KubeProxyConntrackConfiguration, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+
+	if config.Max < 0 {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("Max"), config.Max, "cannot be less than 0"))
+	}
+
+	if config.MaxPerCore < 0 {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("MaxPerCore"), config.MaxPerCore, "cannot be less than 0"))
+	}
+
+	if config.Min < 0 {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("Min"), config.Min, "cannot be less than 0"))
+	}
+
+	if config.TCPEstablishedTimeout.Duration <= 0 {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("TCPEstablishedTimeout"), config.TCPEstablishedTimeout, "must be greater than 0"))
+	}
+
+	if config.TCPCloseWaitTimeout.Duration <= 0 {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("TCPCloseWaitTimeout"), config.TCPCloseWaitTimeout, "must be greater than 0"))
+	}
+
+	return allErrs
+}
+
+func validateProxyMode(mode componentconfig.ProxyMode, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+	if mode != componentconfig.ProxyModeUserspace && mode != componentconfig.ProxyModeIPTables && string(mode) != "" {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("ProxyMode"), string(mode), "must be userspace, iptables or blank, blank means the best-available proxy (currently iptables)"))
+	}
+	return allErrs
+}
---
+++ pkg/apis/componentconfig/validation/validation_test.go
@@ -0,0 +1,214 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package validation
+
+import (
+	"strings"
+	"testing"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	"k8s.io/kubernetes/pkg/apis/componentconfig"
+)
+
+func TestValidateKubeProxyIPTablesConfiguration(t *testing.T) {
+	valid := int32(5)
+	successCases := []componentconfig.KubeProxyIPTablesConfiguration{
+		{
+			MasqueradeAll: true,
+			SyncPeriod:    metav1.Duration{Duration: 5 * time.Second},
+			MinSyncPeriod: metav1.Duration{Duration: 2 * time.Second},
+		},
+		{
+			MasqueradeBit: &valid,
+			MasqueradeAll: true,
+			SyncPeriod:    metav1.Duration{Duration: 5 * time.Second},
+			MinSyncPeriod: metav1.Duration{Duration: 2 * time.Second},
+		},
+	}
+	newPath := field.NewPath("KubeProxyConfiguration")
+	for _, successCase := range successCases {
+		if errs := validateKubeProxyIPTablesConfiguration(successCase, newPath.Child("KubeProxyIPTablesConfiguration")); len(errs) != 0 {
+			t.Errorf("expected success: %v", errs)
+		}
+	}
+
+	invalid := int32(-10)
+	errorCases := []struct {
+		config componentconfig.KubeProxyIPTablesConfiguration
+		msg    string
+	}{
+		{
+			config: componentconfig.KubeProxyIPTablesConfiguration{
+				MasqueradeAll: true,
+				SyncPeriod:    metav1.Duration{Duration: -5 * time.Second},
+				MinSyncPeriod: metav1.Duration{Duration: 2 * time.Second},
+			},
+			msg: "must be greater than 0",
+		},
+		{
+			config: componentconfig.KubeProxyIPTablesConfiguration{
+				MasqueradeBit: &valid,
+				MasqueradeAll: true,
+				SyncPeriod:    metav1.Duration{Duration: 5 * time.Second},
+				MinSyncPeriod: metav1.Duration{Duration: -1 * time.Second},
+			},
+			msg: "cannot be less than 0",
+		},
+		{
+			config: componentconfig.KubeProxyIPTablesConfiguration{
+				MasqueradeBit: &invalid,
+				MasqueradeAll: true,
+				SyncPeriod:    metav1.Duration{Duration: 5 * time.Second},
+				MinSyncPeriod: metav1.Duration{Duration: 2 * time.Second},
+			},
+			msg: "must be within the range [0, 31]",
+		},
+	}
+
+	for _, errorCase := range errorCases {
+		if errs := validateKubeProxyIPTablesConfiguration(errorCase.config, newPath.Child("KubeProxyIPTablesConfiguration")); len(errs) == 0 {
+			t.Errorf("expected failure for %s", errorCase.msg)
+		} else if !strings.Contains(errs[0].Error(), errorCase.msg) {
+			t.Errorf("unexpected error: %v, expected: %s", errs[0], errorCase.msg)
+		}
+	}
+}
+
+func TestValidateKubeProxyConntrackConfiguration(t *testing.T) {
+	successCases := []componentconfig.KubeProxyConntrackConfiguration{
+		{
+			Max:        int32(2),
+			MaxPerCore: int32(1),
+			Min:        int32(1),
+			TCPEstablishedTimeout: metav1.Duration{Duration: 5 * time.Second},
+			TCPCloseWaitTimeout:   metav1.Duration{Duration: 5 * time.Second},
+		},
+		{
+			Max:        0,
+			MaxPerCore: 0,
+			Min:        0,
+			TCPEstablishedTimeout: metav1.Duration{Duration: 5 * time.Second},
+			TCPCloseWaitTimeout:   metav1.Duration{Duration: 60 * time.Second},
+		},
+	}
+	newPath := field.NewPath("KubeProxyConfiguration")
+	for _, successCase := range successCases {
+		if errs := validateKubeProxyConntrackConfiguration(successCase, newPath.Child("KubeProxyConntrackConfiguration")); len(errs) != 0 {
+			t.Errorf("expected success: %v", errs)
+		}
+	}
+
+	errorCases := []struct {
+		config componentconfig.KubeProxyConntrackConfiguration
+		msg    string
+	}{
+		{
+			config: componentconfig.KubeProxyConntrackConfiguration{
+				Max:        int32(-1),
+				MaxPerCore: int32(1),
+				Min:        int32(1),
+				TCPEstablishedTimeout: metav1.Duration{Duration: 5 * time.Second},
+				TCPCloseWaitTimeout:   metav1.Duration{Duration: 5 * time.Second},
+			},
+			msg: "cannot be less than 0",
+		},
+		{
+			config: componentconfig.KubeProxyConntrackConfiguration{
+				Max:        int32(2),
+				MaxPerCore: int32(-1),
+				Min:        int32(1),
+				TCPEstablishedTimeout: metav1.Duration{Duration: 5 * time.Second},
+				TCPCloseWaitTimeout:   metav1.Duration{Duration: 5 * time.Second},
+			},
+			msg: "cannot be less than 0",
+		},
+		{
+			config: componentconfig.KubeProxyConntrackConfiguration{
+				Max:        int32(2),
+				MaxPerCore: int32(1),
+				Min:        int32(-1),
+				TCPEstablishedTimeout: metav1.Duration{Duration: 5 * time.Second},
+				TCPCloseWaitTimeout:   metav1.Duration{Duration: 5 * time.Second},
+			},
+			msg: "cannot be less than 0",
+		},
+		{
+			config: componentconfig.KubeProxyConntrackConfiguration{
+				Max:        int32(4),
+				MaxPerCore: int32(1),
+				Min:        int32(3),
+				TCPEstablishedTimeout: metav1.Duration{Duration: -5 * time.Second},
+				TCPCloseWaitTimeout:   metav1.Duration{Duration: 5 * time.Second},
+			},
+			msg: "must be greater than 0",
+		},
+		{
+			config: componentconfig.KubeProxyConntrackConfiguration{
+				Max:        int32(4),
+				MaxPerCore: int32(1),
+				Min:        int32(3),
+				TCPEstablishedTimeout: metav1.Duration{Duration: 5 * time.Second},
+				TCPCloseWaitTimeout:   metav1.Duration{Duration: -5 * time.Second},
+			},
+			msg: "must be greater than 0",
+		},
+	}
+
+	for _, errorCase := range errorCases {
+		if errs := validateKubeProxyConntrackConfiguration(errorCase.config, newPath.Child("KubeProxyConntrackConfiguration")); len(errs) == 0 {
+			t.Errorf("expected failure for %s", errorCase.msg)
+		} else if !strings.Contains(errs[0].Error(), errorCase.msg) {
+			t.Errorf("unexpected error: %v, expected: %s", errs[0], errorCase.msg)
+		}
+	}
+}
+
+func TestValidateProxyMode(t *testing.T) {
+	newPath := field.NewPath("KubeProxyConfiguration")
+
+	successCases := []componentconfig.ProxyMode{
+		componentconfig.ProxyModeUserspace,
+		componentconfig.ProxyModeIPTables,
+		componentconfig.ProxyMode(""),
+	}
+
+	for _, successCase := range successCases {
+		if errs := validateProxyMode(successCase, newPath.Child("ProxyMode")); len(errs) != 0 {
+			t.Errorf("expected success: %v", errs)
+		}
+	}
+
+	errorCases := []struct {
+		mode componentconfig.ProxyMode
+		msg  string
+	}{
+		{
+			mode: componentconfig.ProxyMode("non-existing"),
+			msg:  "must be userspace, iptables or blank, blank means the best-available proxy (currently iptables)",
+		},
+	}
+
+	for _, errorCase := range errorCases {
+		if errs := validateProxyMode(errorCase.mode, newPath.Child("ProxyMode")); len(errs) == 0 {
+			t.Errorf("expected failure for %s", errorCase.msg)
+		} else if !strings.Contains(errs[0].Error(), errorCase.msg) {
+			t.Errorf("unexpected error: %v, expected: %s", errs[0], errorCase.msg)
+		}
+	}
+}
