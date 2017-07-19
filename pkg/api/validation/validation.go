db74954af95c8c75d4bfee722854a1a858ec7908
Di Xu <stephenhsu90@gmail.com>
2017-07-19 15:48:36 +0800

    set default HostPathType to empty

--- pkg/api/validation/validation.go
+++ pkg/api/validation/validation.go
@@ -944,6 +944,7 @@ func validateProjectedVolumeSource(projection *api.ProjectedVolumeSource, fldPat
 }
 
 var supportedHostPathTypes = sets.NewString(
+	string(api.HostPathUnset),
 	string(api.HostPathDirectoryOrCreate),
 	string(api.HostPathDirectory),
 	string(api.HostPathFileOrCreate),
