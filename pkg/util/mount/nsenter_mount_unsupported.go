7d5c2403e3ae63d95a10dc4dfb1980e8625c3ae1
Jan Safranek <jsafrane@redhat.com>
2017-07-21 15:27:13 +0200

    Add Mounter.MakeShared method
    MakeShared checks that given path is on a mount with 'shared' mount
    propagation. If not, it bind-mounts the path as shared.

--- pkg/util/mount/nsenter_mount_unsupported.go
+++ pkg/util/mount/nsenter_mount_unsupported.go
@@ -61,3 +61,7 @@ func (*NsenterMounter) PathIsDevice(pathname string) (bool, error) {
 func (*NsenterMounter) GetDeviceNameFromMount(mountPath, pluginDir string) (string, error) {
 	return "", nil
 }
+
+func (*NsenterMounter) MakeShared(path string) error {
+	return nil
+}
