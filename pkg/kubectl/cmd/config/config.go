18bcccab91af6eb9bd35b394bc8cb2a5cb61dcb4
Seth Pollack <spollack@beenverified.com>
2017-08-01 13:54:29 -0400

    add prompt for secrets

--- pkg/kubectl/cmd/config/config.go
+++ pkg/kubectl/cmd/config/config.go
@@ -30,7 +30,7 @@ import (
 )
 
 // NewCmdConfig creates a command object for the "config" action, and adds all child commands to it.
-func NewCmdConfig(pathOptions *clientcmd.PathOptions, out, errOut io.Writer) *cobra.Command {
+func NewCmdConfig(pathOptions *clientcmd.PathOptions, in io.Reader, out, errOut io.Writer) *cobra.Command {
 	if len(pathOptions.ExplicitFileFlag) == 0 {
 		pathOptions.ExplicitFileFlag = clientcmd.RecommendedConfigPathFlag
 	}
@@ -54,7 +54,7 @@ func NewCmdConfig(pathOptions *clientcmd.PathOptions, out, errOut io.Writer) *co
 
 	cmd.AddCommand(NewCmdConfigView(out, errOut, pathOptions))
 	cmd.AddCommand(NewCmdConfigSetCluster(out, pathOptions))
-	cmd.AddCommand(NewCmdConfigSetAuthInfo(out, pathOptions))
+	cmd.AddCommand(NewCmdConfigSetAuthInfo(in, out, pathOptions))
 	cmd.AddCommand(NewCmdConfigSetContext(out, pathOptions))
 	cmd.AddCommand(NewCmdConfigSet(out, pathOptions))
 	cmd.AddCommand(NewCmdConfigUnset(out, pathOptions))
