3b97de9573288e7491212af96680e2f20f839c53
Di Xu <stephenhsu90@gmail.com>
2017-08-04 16:44:36 +0800

    rename pkg/kubectl/resource Selector.Selector to Selector.LabelSelector

--- pkg/kubectl/resource/selector.go
+++ pkg/kubectl/resource/selector.go
@@ -30,7 +30,7 @@ type Selector struct {
 	Client               RESTClient
 	Mapping              *meta.RESTMapping
 	Namespace            string
-	Selector             labels.Selector
+	LabelSelector        labels.Selector
 	Export               bool
 	IncludeUninitialized bool
 }
@@ -41,7 +41,7 @@ func NewSelector(client RESTClient, mapping *meta.RESTMapping, namespace string,
 		Client:               client,
 		Mapping:              mapping,
 		Namespace:            namespace,
-		Selector:             selector,
+		LabelSelector:        selector,
 		Export:               export,
 		IncludeUninitialized: includeUninitialized,
 	}
@@ -49,22 +49,22 @@ func NewSelector(client RESTClient, mapping *meta.RESTMapping, namespace string,
 
 // Visit implements Visitor
 func (r *Selector) Visit(fn VisitorFunc) error {
-	list, err := NewHelper(r.Client, r.Mapping).List(r.Namespace, r.ResourceMapping().GroupVersionKind.GroupVersion().String(), r.Selector, r.Export, r.IncludeUninitialized)
+	list, err := NewHelper(r.Client, r.Mapping).List(r.Namespace, r.ResourceMapping().GroupVersionKind.GroupVersion().String(), r.LabelSelector, r.Export, r.IncludeUninitialized)
 	if err != nil {
 		if errors.IsBadRequest(err) || errors.IsNotFound(err) {
 			if se, ok := err.(*errors.StatusError); ok {
 				// modify the message without hiding this is an API error
-				if r.Selector.Empty() {
+				if r.LabelSelector.Empty() {
 					se.ErrStatus.Message = fmt.Sprintf("Unable to list %q: %v", r.Mapping.Resource, se.ErrStatus.Message)
 				} else {
-					se.ErrStatus.Message = fmt.Sprintf("Unable to find %q that match the selector %q: %v", r.Mapping.Resource, r.Selector, se.ErrStatus.Message)
+					se.ErrStatus.Message = fmt.Sprintf("Unable to find %q that match the selector %q: %v", r.Mapping.Resource, r.LabelSelector, se.ErrStatus.Message)
 				}
 				return se
 			}
-			if r.Selector.Empty() {
+			if r.LabelSelector.Empty() {
 				return fmt.Errorf("Unable to list %q: %v", r.Mapping.Resource, err)
 			} else {
-				return fmt.Errorf("Unable to find %q that match the selector %q: %v", r.Mapping.Resource, r.Selector, err)
+				return fmt.Errorf("Unable to find %q that match the selector %q: %v", r.Mapping.Resource, r.LabelSelector, err)
 			}
 		}
 		return err
@@ -83,7 +83,7 @@ func (r *Selector) Visit(fn VisitorFunc) error {
 }
 
 func (r *Selector) Watch(resourceVersion string) (watch.Interface, error) {
-	return NewHelper(r.Client, r.Mapping).Watch(r.Namespace, resourceVersion, r.ResourceMapping().GroupVersionKind.GroupVersion().String(), r.Selector)
+	return NewHelper(r.Client, r.Mapping).Watch(r.Namespace, resourceVersion, r.ResourceMapping().GroupVersionKind.GroupVersion().String(), r.LabelSelector)
 }
 
 // ResourceMapping returns the mapping for this resource and implements ResourceMapping
