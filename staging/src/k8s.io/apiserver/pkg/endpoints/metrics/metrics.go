8d9eaac2e96c683a3c8cc4e0cf2639e214f287a2
Chenxingyu <chenxingyu@qiniu.com>
2017-08-04 11:08:36 +0800

    make get pod log with follow option as watch verb

--- staging/src/k8s.io/apiserver/pkg/endpoints/metrics/metrics.go
+++ staging/src/k8s.io/apiserver/pkg/endpoints/metrics/metrics.go
@@ -107,6 +107,15 @@ func MonitorRequest(request *http.Request, verb, resource, subresource, scope, c
 		}
 	}
 
+	// make get pod log with follow option api request reported as watch
+	if verb == "GET" && resource == "pods" && subresource == "log" {
+		if values := request.URL.Query()["follow"]; len(values) > 0 {
+			if value := strings.ToLower(values[0]); value == "true" {
+				reportedVerb = "WATCH"
+			}
+		}
+	}
+
 	client := cleanUserAgent(utilnet.GetHTTPClient(request))
 	Monitor(reportedVerb, resource, subresource, scope, client, contentType, httpCode, respSize, reqStart)
 }
