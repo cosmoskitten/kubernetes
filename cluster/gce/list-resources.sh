90c5cd130fecc414b017e58fe9e93015f076f6ee
Philip Ingrey <me@philingrey.com>
2017-08-09 17:45:44 +0100

    Switch away from gcloud deprecated flags in compute resource listings

--- cluster/gce/list-resources.sh
+++ cluster/gce/list-resources.sh
@@ -73,21 +73,21 @@ echo "Provider: ${KUBERNETES_PROVIDER:-}"
 
 # List resources related to instances, filtering by the instance prefix if
 # provided.
-gcloud-compute-list instance-templates --regexp="${INSTANCE_PREFIX}.*"
-gcloud-compute-list instance-groups ${ZONE:+"--zones=${ZONE}"} --regexp="${INSTANCE_PREFIX}.*"
-gcloud-compute-list instances ${ZONE:+"--zones=${ZONE}"} --regexp="${INSTANCE_PREFIX}.*"
+gcloud-compute-list instance-templates --filter="name ~ '${INSTANCE_PREFIX}.*'"
+gcloud-compute-list instance-groups --filter="${ZONE:+"zone=(${ZONE}) AND"} name ~ '${INSTANCE_PREFIX}.*'"
+gcloud-compute-list instances --filter="${ZONE:+"zone=(${ZONE}) AND"} name ~ '${INSTANCE_PREFIX}.*'"
 
 # List disk resources, filterying by instance prefix if provided.
-gcloud-compute-list disks ${ZONE:+"--zones=${ZONE}"} --regexp="${INSTANCE_PREFIX}.*"
+gcloud-compute-list disks --filter="${ZONE:+"zone=(${ZONE}) AND"} name ~ '${INSTANCE_PREFIX}.*'"
 
 # List network resources. We include names starting with "a", corresponding to
 # those that Kubernetes creates.
-gcloud-compute-list addresses ${REGION:+"--regions=${REGION}"} --regexp="a.*|${INSTANCE_PREFIX}.*"
+gcloud-compute-list addresses ${REGION:+"--regions=${REGION}"} --filter="name ~ 'a.*|${INSTANCE_PREFIX}.*'"
 # Match either the header or a line with the specified e2e network.
 # This assumes that the network name is the second field in the output.
 GREP_REGEX="^NAME\|^[^ ]\+[ ]\+\(default\|${NETWORK}\) "
-gcloud-compute-list routes --regexp="default.*|${INSTANCE_PREFIX}.*"
-gcloud-compute-list firewall-rules --regexp="default.*|k8s-fw.*|${INSTANCE_PREFIX}.*"
+gcloud-compute-list routes --filter="name ~ 'default.*|${INSTANCE_PREFIX}.*'"
+gcloud-compute-list firewall-rules --filter="name ~ 'default.*|k8s-fw.*|${INSTANCE_PREFIX}.*'"
 GREP_REGEX=""
 gcloud-compute-list forwarding-rules ${REGION:+"--regions=${REGION}"}
 gcloud-compute-list target-pools ${REGION:+"--regions=${REGION}"}
