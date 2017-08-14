8fc789388c22306f72bfff94c624d7c60dec3295
Shiyang Wang <shiywang@redhat.com>
2017-08-14 16:24:30 +0800

    Fix Content negotiation incorrect when Accept header uses type parameters

--- staging/src/k8s.io/apiserver/pkg/endpoints/handlers/negotiation/negotiate.go
+++ staging/src/k8s.io/apiserver/pkg/endpoints/handlers/negotiation/negotiate.go
@@ -263,6 +263,33 @@ func acceptMediaTypeOptions(params map[string]string, accepts *AcceptedMediaType
 	return options, true
 }
 
+type candidateMediaType struct {
+	accepted *AcceptedMediaType
+	clauses  goautoneg.Accept
+}
+
+func mostSpecificMediaType(l []candidateMediaType) candidateMediaType {
+	max :=  struct{
+		len int
+		index int
+	} {
+		len: 0,
+		index: 0,
+	}
+	for i, val := range l {
+		paramLen := 0
+		for k, v := range val.clauses.Params {
+			paramLen = paramLen + len(k) + len(v)
+		}
+		specLen := len(val.clauses.Type) + len(val.clauses.SubType) + paramLen
+		if specLen > max.len {
+			max.len = specLen
+			max.index = i
+		}
+	}
+	return l[max.index]
+}
+
 // NegotiateMediaTypeOptions returns the most appropriate content type given the accept header and
 // a list of alternatives along with the accepted media type parameters.
 func NegotiateMediaTypeOptions(header string, accepted []AcceptedMediaType, endpoint EndpointRestrictions) (MediaTypeOptions, bool) {
@@ -273,6 +300,7 @@ func NegotiateMediaTypeOptions(header string, accepted []AcceptedMediaType, endp
 	}
 
 	clauses := goautoneg.ParseAccept(header)
+	delegationMediaTypeList := []candidateMediaType{}
 	for _, clause := range clauses {
 		for i := range accepted {
 			accepts := &accepted[i]
@@ -282,10 +310,16 @@ func NegotiateMediaTypeOptions(header string, accepted []AcceptedMediaType, endp
 				clause.Type == "*" && clause.SubType == "*":
 				// TODO: should we prefer the first type with no unrecognized options?  Do we need to ignore unrecognized
 				// parameters.
-				return acceptMediaTypeOptions(clause.Params, accepts, endpoint)
+				delegationMediaTypeList = append(delegationMediaTypeList, candidateMediaType{accepted: accepts, clauses: clause})
 			}
 		}
 	}
+
+	if len(delegationMediaTypeList) > 0 {
+		ret := mostSpecificMediaType(delegationMediaTypeList)
+		return acceptMediaTypeOptions(ret.clauses.Params, ret.accepted, endpoint)
+	}
+
 	return MediaTypeOptions{}, false
 }
 
