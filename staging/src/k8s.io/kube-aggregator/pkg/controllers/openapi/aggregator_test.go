dd8abf84d3b871adce7d26d88298d7c88902869c
mbohlool <mehdy@google.com>
2017-08-22 17:55:34 -0700

    Refactor OpenAPI aggregator to have separate controller and aggregation function
    Enable OpenAPI spec for extensions api server
    
    Do not filter paths. higher priority specs wins the conflicting paths
    
    Move OpenAPI aggregation controller to pkg/controller/openapi

--- staging/src/k8s.io/kube-aggregator/pkg/controllers/openapi/aggregator_test.go
+++ staging/src/k8s.io/kube-aggregator/pkg/controllers/openapi/aggregator_test.go
@@ -14,24 +14,27 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package apiserver
+package openapi
 
 import (
+	"fmt"
 	"net/http"
 	"reflect"
 	"testing"
 
 	"github.com/go-openapi/spec"
+	"github.com/stretchr/testify/assert"
 
 	"k8s.io/apiserver/pkg/endpoints/request"
 	"k8s.io/kube-aggregator/pkg/apis/apiregistration"
 )
 
-func newApiServiceForTest(name, group string, minGroupPriority, versionPriority int32) apiregistration.APIService {
+func newAPIServiceForTest(name, group string, minGroupPriority, versionPriority int32) apiregistration.APIService {
 	r := apiregistration.APIService{}
 	r.Spec.Group = group
 	r.Spec.GroupPriorityMinimum = minGroupPriority
 	r.Spec.VersionPriority = versionPriority
+	r.Spec.Service = &apiregistration.ServiceReference{}
 	r.Name = name
 	return r
 }
@@ -46,22 +49,22 @@ func assertSortedServices(t *testing.T, actual []openAPISpecInfo, expectedNames 
 	}
 }
 
-func TestApiServiceSort(t *testing.T) {
+func TestAPIServiceSort(t *testing.T) {
 	list := []openAPISpecInfo{
 		{
-			apiService: newApiServiceForTest("FirstService", "Group1", 10, 5),
+			apiService: newAPIServiceForTest("FirstService", "Group1", 10, 5),
 			spec:       &spec.Swagger{},
 		},
 		{
-			apiService: newApiServiceForTest("SecondService", "Group2", 15, 3),
+			apiService: newAPIServiceForTest("SecondService", "Group2", 15, 3),
 			spec:       &spec.Swagger{},
 		},
 		{
-			apiService: newApiServiceForTest("FirstServiceInternal", "Group1", 16, 3),
+			apiService: newAPIServiceForTest("FirstServiceInternal", "Group1", 16, 3),
 			spec:       &spec.Swagger{},
 		},
 		{
-			apiService: newApiServiceForTest("ThirdService", "Group3", 15, 3),
+			apiService: newAPIServiceForTest("ThirdService", "Group3", 15, 3),
 			spec:       &spec.Swagger{},
 		},
 	}
@@ -90,42 +93,43 @@ func (h handlerTest) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write(h.data)
 }
 
-func assertDownloadedSpec(t *testing.T, actualSpec *spec.Swagger, actualEtag string, err error,
-	expectedSpecId string, expectedEtag string) {
+func assertDownloadedSpec(actualSpec *spec.Swagger, actualEtag string, err error,
+	expectedSpecID string, expectedEtag string) error {
 	if err != nil {
-		t.Errorf("downloadOpenAPISpec failed : %s", err)
+		return fmt.Errorf("downloadOpenAPISpec failed : %s", err)
 	}
-	if expectedSpecId == "" && actualSpec != nil {
-		t.Errorf("expected Not Modified, actual ID %s", actualSpec.ID)
+	if expectedSpecID == "" && actualSpec != nil {
+		return fmt.Errorf("expected Not Modified, actual ID %s", actualSpec.ID)
 	}
-	if actualSpec != nil && actualSpec.ID != expectedSpecId {
-		t.Errorf("expected ID %s, actual ID %s", expectedSpecId, actualSpec.ID)
+	if actualSpec != nil && actualSpec.ID != expectedSpecID {
+		return fmt.Errorf("expected ID %s, actual ID %s", expectedSpecID, actualSpec.ID)
 	}
 	if actualEtag != expectedEtag {
-		t.Errorf("expected ETag '%s', actual ETag '%s'", expectedEtag, actualEtag)
+		return fmt.Errorf("expected ETag '%s', actual ETag '%s'", expectedEtag, actualEtag)
 	}
+	return nil
 }
 
 func TestDownloadOpenAPISpec(t *testing.T) {
 
-	s := openAPIAggregator{contextMapper: request.NewRequestContextMapper()}
+	s := Downloader{contextMapper: request.NewRequestContextMapper()}
 
 	// Test with no eTag
-	actualSpec, actualEtag, _, err := s.downloadOpenAPISpec(handlerTest{data: []byte("{\"id\": \"test\"}")}, "")
-	assertDownloadedSpec(t, actualSpec, actualEtag, err, "test", "\"356ECAB19D7FBE1336BABB1E70F8F3025050DE218BE78256BE81620681CFC9A268508E542B8B55974E17B2184BBFC8FFFAA577E51BE195D32B3CA2547818ABE4\"")
+	actualSpec, actualEtag, _, err := s.Download(handlerTest{data: []byte("{\"id\": \"test\"}")}, "")
+	assert.NoError(t, assertDownloadedSpec(actualSpec, actualEtag, err, "test", "\"6E8F849B434D4B98A569B9D7718876E9-356ECAB19D7FBE1336BABB1E70F8F3025050DE218BE78256BE81620681CFC9A268508E542B8B55974E17B2184BBFC8FFFAA577E51BE195D32B3CA2547818ABE4\""))
 
 	// Test with eTag
-	actualSpec, actualEtag, _, err = s.downloadOpenAPISpec(
+	actualSpec, actualEtag, _, err = s.Download(
 		handlerTest{data: []byte("{\"id\": \"test\"}"), etag: "etag_test"}, "")
-	assertDownloadedSpec(t, actualSpec, actualEtag, err, "test", "etag_test")
+	assert.NoError(t, assertDownloadedSpec(actualSpec, actualEtag, err, "test", "etag_test"))
 
 	// Test not modified
-	actualSpec, actualEtag, _, err = s.downloadOpenAPISpec(
+	actualSpec, actualEtag, _, err = s.Download(
 		handlerTest{data: []byte("{\"id\": \"test\"}"), etag: "etag_test"}, "etag_test")
-	assertDownloadedSpec(t, actualSpec, actualEtag, err, "", "etag_test")
+	assert.NoError(t, assertDownloadedSpec(actualSpec, actualEtag, err, "", "etag_test"))
 
 	// Test different eTags
-	actualSpec, actualEtag, _, err = s.downloadOpenAPISpec(
+	actualSpec, actualEtag, _, err = s.Download(
 		handlerTest{data: []byte("{\"id\": \"test\"}"), etag: "etag_test1"}, "etag_test2")
-	assertDownloadedSpec(t, actualSpec, actualEtag, err, "test", "etag_test1")
+	assert.NoError(t, assertDownloadedSpec(actualSpec, actualEtag, err, "test", "etag_test1"))
 }
