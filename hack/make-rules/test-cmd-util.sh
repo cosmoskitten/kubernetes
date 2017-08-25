9fe99557b70298bed11ca28ad4eede2b8324a540
Di Xu <stephenhsu90@gmail.com>
2017-08-25 14:32:05 +0800

    test fix

--- hack/make-rules/test-cmd-util.sh
+++ hack/make-rules/test-cmd-util.sh
@@ -4655,7 +4655,7 @@ run_initializer_tests() {
   kube::log::status "Testing --include-uninitialized"
 
   ### Create a deployment
-  kubectl create -f hack/testdata/initializer-deployments.yaml 2>&1 "${kube_flags[@]}" || true
+  kubectl create --request-timeout=1 -f hack/testdata/initializer-deployments.yaml 2>&1 "${kube_flags[@]}" || true
 
   ### Test kubectl get --include-uninitialized
   # Command
@@ -4668,16 +4668,16 @@ run_initializer_tests() {
   kube::test::if_has_string "${output_message}" 'No resources found'
   # Command
   output_message=$(kubectl get deployments --include-uninitialized 2>&1 "${kube_flags[@]}")
-  # Post-condition: The text "No resources found" should not be part of the output
-  kube::test::if_has_not_string "${output_message}" 'No resources found'
+  # Post-condition: I assume "web" is the deployment name
+  kube::test::if_has_string "${output_message}" 'web'
   # Command
   output_message=$(kubectl get deployments --include-uninitialized=true 2>&1 "${kube_flags[@]}")
-  # Post-condition: The text "No resources found" should not be part of the output
-  kube::test::if_has_not_string "${output_message}" 'No resources found'
+  # Post-condition: I assume "web" is the deployment name
+  kube::test::if_has_string "${output_message}" 'web'
   # Command
   output_message=$(kubectl get deployments web 2>&1 "${kube_flags[@]}")
-  # Post-condition: The text "No resources found" should not be part of the output
-  kube::test::if_has_not_string "${output_message}" 'No resources found'
+  # Post-condition: I assume "web" is the deployment name
+  kube::test::if_has_string "${output_message}" 'web'
   # Command
   output_message=$(kubectl get deployments --show-all 2>&1 "${kube_flags[@]}")
   # Post-condition: The text "No resources found" should be part of the output
@@ -4709,6 +4709,7 @@ run_initializer_tests() {
   # Command
   output_message=$(kubectl label deployments labelkey1=labelvalue1 --all 2>&1 "${kube_flags[@]}")
   # Post-condition: web is labelled
+  kube::test::if_has_string "${output_message}" 'deployment "web" labeled'
   kube::test::get_object_assert 'deployments web' "{{${labels_field}.labelkey1}}" 'labelvalue1'
   # Command
   output_message=$(kubectl label deployments labelkey2=labelvalue2 --all --include-uninitialized=false 2>&1 "${kube_flags[@]}")
@@ -4721,6 +4722,7 @@ run_initializer_tests() {
   # Command
   output_message=$(kubectl label deployments labelkey4=labelvalue4 -l run=web --include-uninitialized 2>&1 "${kube_flags[@]}")
   # Post-condition: web is labelled
+  kube::test::if_has_string "${output_message}" 'deployment "web" labeled'
   kube::test::get_object_assert 'deployments web' "{{${labels_field}.labelkey4}}" 'labelvalue4'
   # Command
   output_message=$(kubectl label deployments labelkey5=labelvalue5 -l run=web --all 2>&1 "${kube_flags[@]}")
@@ -4729,16 +4731,21 @@ run_initializer_tests() {
   # Command
   output_message=$(kubectl label deployments labelkey6=labelvalue6 -l run=web --all --include-uninitialized 2>&1 "${kube_flags[@]}")
   # Post-condition: web is labelled
+  kube::test::if_has_string "${output_message}" 'deployment "web" labeled'
   kube::test::get_object_assert 'deployments web' "{{${labels_field}.labelkey6}}" 'labelvalue6'
   # Command
   output_message=$(kubectl label deployments web labelkey7=labelvalue7 2>&1 "${kube_flags[@]}")
   # Post-condition: web is labelled
+  kube::test::if_has_string "${output_message}" 'deployment "web" labeled'
   kube::test::get_object_assert 'deployments web' "{{${labels_field}.labelkey7}}" 'labelvalue7'
+  # Found All Labels
+  kube::test::get_object_assert 'deployments web' "{{${labels_field}}}" 'map[labelkey1:labelvalue1 labelkey4:labelvalue4 labelkey6:labelvalue6 labelkey7:labelvalue7 run:web]'
 
   ### Test kubectl annotate --include-uninitialized
   # Command
   output_message=$(kubectl annotate deployments annotatekey1=annotatevalue1 --all 2>&1 "${kube_flags[@]}")
   # Post-condition: DEPLOYMENT has annotation
+  kube::test::if_has_string "${output_message}" 'deployment "web" annotated'
   kube::test::get_object_assert 'deployments web' "{{${annotations_field}.annotatekey1}}" 'annotatevalue1'
   # Command
   output_message=$(kubectl annotate deployments annotatekey2=annotatevalue2 --all --include-uninitialized=false 2>&1 "${kube_flags[@]}")
@@ -4751,6 +4758,7 @@ run_initializer_tests() {
   # Command
   output_message=$(kubectl annotate deployments annotatekey4=annotatevalue4 -l run=web --include-uninitialized 2>&1 "${kube_flags[@]}")
   # Post-condition: DEPLOYMENT has annotation
+  kube::test::if_has_string "${output_message}" 'deployment "web" annotated'
   kube::test::get_object_assert 'deployments web' "{{${annotations_field}.annotatekey4}}" 'annotatevalue4'
   # Command
   output_message=$(kubectl annotate deployments annotatekey5=annotatevalue5 -l run=web --all 2>&1 "${kube_flags[@]}")
@@ -4759,10 +4767,12 @@ run_initializer_tests() {
   # Command
   output_message=$(kubectl annotate deployments annotatekey6=annotatevalue6 -l run=web --all --include-uninitialized 2>&1 "${kube_flags[@]}")
   # Post-condition: DEPLOYMENT has annotation
+  kube::test::if_has_string "${output_message}" 'deployment "web" annotated'
   kube::test::get_object_assert 'deployments web' "{{${annotations_field}.annotatekey6}}" 'annotatevalue6'
   # Command
   output_message=$(kubectl annotate deployments web annotatekey7=annotatevalue7 2>&1 "${kube_flags[@]}")
   # Post-condition: web DEPLOYMENT has annotation
+  kube::test::if_has_string "${output_message}" 'deployment "web" annotated'
   kube::test::get_object_assert 'deployments web' "{{${annotations_field}.annotatekey7}}" 'annotatevalue7'
 
   ### Test kubectl edit --include-uninitialized
@@ -4815,7 +4825,7 @@ run_initializer_tests() {
 
   ### Test kubectl set selector --include-uninitialized
   # Create a service with initializer
-  kubectl create -f hack/testdata/initializer-redis-master-service.yaml 2>&1 "${kube_flags[@]}" || true
+  kubectl create --request-timeout=1 -f hack/testdata/initializer-redis-master-service.yaml 2>&1 "${kube_flags[@]}" || true
   # Command
   output_message=$(kubectl set selector services role=padawan --all 2>&1 "${kube_flags[@]}")
   # Post-condition: The text "selector updated" should be part of the output
@@ -4827,7 +4837,7 @@ run_initializer_tests() {
 
   ### Test kubectl set subject --include-uninitialized
   # Create a create clusterrolebinding with initializer
-  kubectl create -f hack/testdata/initializer-clusterrolebinding.yaml 2>&1 "${kube_flags[@]}" || true
+  kubectl create --request-timeout=1 -f hack/testdata/initializer-clusterrolebinding.yaml 2>&1 "${kube_flags[@]}" || true
   kube::test::get_object_assert clusterrolebinding/super-admin "{{range.subjects}}{{.name}}:{{end}}" 'super-admin:'
   # Command
   output_message=$(kubectl set subject clusterrolebinding --user=foo --all 2>&1 "${kube_flags[@]}")
@@ -4889,10 +4899,12 @@ run_initializer_tests() {
   # check right pod exists
   kube::test::get_object_assert pods/a "{{${id_field}}}" 'a'
   # Post-condition: Other resources should be pruned
-  kube::test::get_object_assert deployments "{{range.items}}{{$id_field}}:{{end}}" ''
-  kube::test::get_object_assert services/redis-master "{{range.items}}{{$id_field}}:{{end}}" ''
+  kube::test::get_object_assert deployments/web "{{range.items}}{{$id_field}}:{{end}}" 'web'
+  kube::test::get_object_assert services/redis-master "{{range.items}}{{$id_field}}:{{end}}" 'redis-master'
   # cleanup
   kubectl delete pod a
+  kubectl delete --request-timeout=1 deploy web
+  kubectl delete --request-timeout=1 service redis-master
 
   set +o nounset
   set +o errexit
