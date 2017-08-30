0662c58425b08b5ad4656689be1309bc748dc687
WanLinghao <wlh834532254@qq.com>
2017-08-30 21:29:39 +0800

    modified:   pkg/kubectl/cmd/cp.go
    fix kubectl cp command error.
    it happens when copy directory to pod and the directory
    path ends with '/'.
    for example:
    kubectl cp /XX/XX/ XX-POD:/XX/XX
    
    modified:   pkg/kubectl/cmd/cp.go

--- pkg/kubectl/cmd/cp.go
+++ pkg/kubectl/cmd/cp.go
@@ -195,6 +195,11 @@ func makeTar(filepath string, writer io.Writer) error {
 	// TODO: use compression here?
 	tarWriter := tar.NewWriter(writer)
 	defer tarWriter.Close()
+
+	len := len(filepath)
+	if filepath[len-1] == '/' {
+		filepath = filepath[0 : len-1]
+	}
 	return recursiveTar(path.Dir(filepath), path.Base(filepath), tarWriter)
 }
 
