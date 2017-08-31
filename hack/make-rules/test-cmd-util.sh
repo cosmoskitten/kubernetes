41c87994e65a6555597a926260c625825a57d3c4
Di Xu <stephenhsu90@gmail.com>
2017-08-31 14:24:24 +0800

    add tests

--- hack/make-rules/test-cmd-util.sh
+++ hack/make-rules/test-cmd-util.sh
@@ -2816,7 +2816,7 @@ run_deployment_tests() {
   ! kubectl rollout status deployment/nginx --revision=3
   cat hack/testdata/deployment-revision1.yaml | $SED "s/name: nginx$/name: nginx2/" | kubectl create -f - "${kube_flags[@]}"
   # Deletion of both deployments should not be blocked
-   kubectl delete deployment nginx2 "${kube_flags[@]}"
+  kubectl delete deployment nginx2 "${kube_flags[@]}"
   # Clean up
   kubectl delete deployment nginx "${kube_flags[@]}"
 
@@ -2854,7 +2854,6 @@ run_deployment_tests() {
   kubectl set image deployment nginx-deployment "*"="${IMAGE_DEPLOYMENT_R1}" "${kube_flags[@]}"
   kube::test::get_object_assert deployment "{{range.items}}{{$image_field0}}:{{end}}" "${IMAGE_DEPLOYMENT_R1}:"
   kube::test::get_object_assert deployment "{{range.items}}{{$image_field1}}:{{end}}" "${IMAGE_DEPLOYMENT_R1}:"
-
   # Clean up
   kubectl delete deployment nginx-deployment "${kube_flags[@]}"
 
@@ -2882,6 +2881,16 @@ run_deployment_tests() {
   kubectl delete deployment nginx-deployment "${kube_flags[@]}"
   kubectl delete configmap test-set-env-config "${kube_flags[@]}"
   kubectl delete secret test-set-env-secret "${kube_flags[@]}"
+
+  ### Delete a deployment with initializer
+  # Pre-condition: no deployment exists
+  kube::test::get_object_assert deployment "{{range.items}}{{$id_field}}:{{end}}" ''
+  # Create a deployment
+  kubectl create -f hack/testdata/deployment-with-initializer.yaml "${kube_flags[@]}"
+  kube::test::get_object_assert 'deployment web' "{{$id_field}}" 'web'
+  # Delete a deployment
+  kubectl delete deployment web "${kube_flags[@]}"
+  kube::test::get_object_assert 'deployment web' "{{$id_field}}" ''
 
   set +o nounset
   set +o errexit
@@ -2994,6 +3003,16 @@ run_rs_tests() {
   kubectl delete rs frontend redis-slave "${kube_flags[@]}" # delete multiple replica sets at once
   # Post-condition: no replica set exists
   kube::test::get_object_assert rs "{{range.items}}{{$id_field}}:{{end}}" ''
+
+  ### Delete a rs with initializer
+  # Pre-condition: no rs exists
+  kube::test::get_object_assert rs "{{range.items}}{{$id_field}}:{{end}}" ''
+  # Create a rs
+  kubectl create -f hack/testdata/replicaset-with-initializer.yaml "${kube_flags[@]}"
+  kube::test::get_object_assert 'rs nginx' "{{$id_field}}" 'nginx'
+  # Delete a rs
+  kubectl delete rs nginx "${kube_flags[@]}"
+  kube::test::get_object_assert 'rs nginx' "{{$id_field}}" ''
 
   if kube::test::if_supports_resource "${horizontalpodautoscalers}" ; then
     ### Auto scale replica set
