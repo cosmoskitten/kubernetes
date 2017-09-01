1643d3f374057a3c9b7697e04a77f291d48cf7b4
zhangjie <zhangjie0619@yeah.net>
2017-09-01 17:30:24 +0800

    move getMaxVols function to predicates.go and add some NewVolumeCountPredicate funcs
    Signed-off-by: zhangjie <zhangjie0619@yeah.net>

--- plugin/pkg/scheduler/algorithm/predicates/predicates.go
+++ plugin/pkg/scheduler/algorithm/predicates/predicates.go
@@ -33,6 +33,7 @@ import (
 	"k8s.io/client-go/util/workqueue"
 	v1helper "k8s.io/kubernetes/pkg/api/v1/helper"
 	v1qos "k8s.io/kubernetes/pkg/api/v1/helper/qos"
+	"k8s.io/kubernetes/pkg/cloudprovider/providers/aws"
 	"k8s.io/kubernetes/pkg/features"
 	kubeletapis "k8s.io/kubernetes/pkg/kubelet/apis"
 	volumeutil "k8s.io/kubernetes/pkg/volume/util"
@@ -43,6 +44,19 @@ import (
 	"k8s.io/metrics/pkg/client/clientset_generated/clientset"
 
 	"github.com/golang/glog"
+	"os"
+)
+
+const (
+	// DefaultMaxGCEPDVolumes defines the maximum number of PD Volumes for GCE
+	// GCE instances can have up to 16 PD volumes attached.
+	DefaultMaxGCEPDVolumes = 16
+	// DefaultMaxAzureDiskVolumes defines the maximum number of PD Volumes for Azure
+	// Larger Azure VMs can actually have much more disks attached.
+	// TODO We should determine the max based on VM size
+	DefaultMaxAzureDiskVolumes = 16
+	// KubeMaxPDVols defines the maximum number of PD Volumes per kubelet
+	KubeMaxPDVols = "KUBE_MAX_PD_VOLS"
 )
 
 // NodeInfo: Other types for predicate functions...
@@ -177,6 +191,16 @@ type VolumeFilter struct {
 	FilterPersistentVolume func(pv *v1.PersistentVolume) (id string, relevant bool)
 }
 
+func NewMaxEBSVolumeCountPredicate(pvInfo PersistentVolumeInfo, pvcInfo PersistentVolumeClaimInfo) algorithm.FitPredicate {
+	return newMaxPDVolumeCountPredicate(EBSVolumeFilter, getMaxVols(aws.DefaultMaxEBSVolumes), pvInfo, pvcInfo)
+}
+func NewMaxGCEPDVolumeCountPredicate(pvInfo PersistentVolumeInfo, pvcInfo PersistentVolumeClaimInfo) algorithm.FitPredicate {
+	return newMaxPDVolumeCountPredicate(GCEPDVolumeFilter, getMaxVols(DefaultMaxGCEPDVolumes), pvInfo, pvcInfo)
+}
+func NewMaxAzureDiskVolumeCountPredicate(pvInfo PersistentVolumeInfo, pvcInfo PersistentVolumeClaimInfo) algorithm.FitPredicate {
+	return newMaxPDVolumeCountPredicate(AzureDiskVolumeFilter, getMaxVols(DefaultMaxAzureDiskVolumes), pvInfo, pvcInfo)
+}
+
 // NewMaxPDVolumeCountPredicate creates a predicate which evaluates whether a pod can fit based on the
 // number of volumes which match a filter that it requests, and those that are already present.  The
 // maximum number is configurable to accommodate different systems.
@@ -184,7 +208,7 @@ type VolumeFilter struct {
 // The predicate looks for both volumes used directly, as well as PVC volumes that are backed by relevant volume
 // types, counts the number of unique volumes, and rejects the new pod if it would place the total count over
 // the maximum.
-func NewMaxPDVolumeCountPredicate(filter VolumeFilter, maxVolumes int, pvInfo PersistentVolumeInfo, pvcInfo PersistentVolumeClaimInfo) algorithm.FitPredicate {
+func newMaxPDVolumeCountPredicate(filter VolumeFilter, maxVolumes int, pvInfo PersistentVolumeInfo, pvcInfo PersistentVolumeClaimInfo) algorithm.FitPredicate {
 	c := &MaxPDVolumeCountChecker{
 		filter:     filter,
 		maxVolumes: maxVolumes,
@@ -193,6 +217,21 @@ func NewMaxPDVolumeCountPredicate(filter VolumeFilter, maxVolumes int, pvInfo Pe
 	}
 
 	return c.predicate
+}
+
+// getMaxVols checks the max PD volumes environment variable, otherwise returning a default value
+func getMaxVols(defaultVal int) int {
+	if rawMaxVols := os.Getenv(KubeMaxPDVols); rawMaxVols != "" {
+		if parsedMaxVols, err := strconv.Atoi(rawMaxVols); err != nil {
+			glog.Errorf("Unable to parse maximum PD volumes value, using default of %v: %v", defaultVal, err)
+		} else if parsedMaxVols <= 0 {
+			glog.Errorf("Maximum PD volumes must be a positive value, using default of %v", defaultVal)
+		} else {
+			return parsedMaxVols
+		}
+	}
+
+	return defaultVal
 }
 
 func (c *MaxPDVolumeCountChecker) filterVolumes(volumes []v1.Volume, namespace string, filteredVolumes map[string]bool) error {
