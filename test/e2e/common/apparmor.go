3a30aad52832fcb530718f7b969f371999de5aff
Di Xu <stephenhsu90@gmail.com>
2017-09-13 15:20:18 +0800

    enable to specific unconfined AppArmor profile

--- test/e2e/common/apparmor.go
+++ test/e2e/common/apparmor.go
@@ -52,7 +52,7 @@ func LoadAppArmorProfiles(f *framework.Framework) {
 // CreateAppArmorTestPod creates a pod that tests apparmor profile enforcement. The pod exits with
 // an error code if the profile is incorrectly enforced. If runOnce is true the pod will exit after
 // a single test, otherwise it will repeat the test every 1 second until failure.
-func CreateAppArmorTestPod(f *framework.Framework, runOnce bool) *api.Pod {
+func CreateAppArmorTestPod(f *framework.Framework, unconfined bool, runOnce bool) *api.Pod {
 	profile := "localhost/" + appArmorProfilePrefix + f.Namespace.Name
 	testCmd := fmt.Sprintf(`
 if touch %[1]s; then
@@ -66,6 +66,18 @@ elif ! grep "%[3]s" /proc/self/attr/current; then
   echo "found: $(cat /proc/self/attr/current)"
   exit 3
 fi`, appArmorDeniedPath, appArmorAllowedPath, appArmorProfilePrefix+f.Namespace.Name)
+
+	if unconfined {
+		profile = apparmor.ProfileNameUnconfined
+		testCmd = `
+if cat /proc/sysrq-trigger | grep "Permission denied"; then
+  echo "FAILURE: reading /proc/sysrq-trigger should be allowed"
+  exit 1
+elif ! grep unconfined /proc/self/attr/current; then
+  echo "FAILURE: not running with expected profile unconfined"
+  exit 2
+fi`
+	}
 
 	if !runOnce {
 		testCmd = fmt.Sprintf(`while true; do
