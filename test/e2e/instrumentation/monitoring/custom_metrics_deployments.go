21f4130316add8e6be9178edf3ce1ceeac0cdb8c
Karol Wychowaniec <kawych@google.com>
2017-09-25 17:01:46 +0200

    Add test for HPA

--- test/e2e/instrumentation/monitoring/custom_metrics_deployments.go
+++ test/e2e/instrumentation/monitoring/custom_metrics_deployments.go
@@ -18,7 +18,8 @@ package monitoring
 
 import (
 	"fmt"
-	"k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
+	extensions "k8s.io/api/extensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -27,65 +28,64 @@ var (
 	UnusedMetricName = "unused-metric"
 	MetricValue1     = int64(448)
 	MetricValue2     = int64(446)
-
-	SDExporterPod1 = &v1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "sd-exporter-1",
-			Namespace: "default",
-			Labels: map[string]string{
-				"name": "sd-exporter",
-			},
-		},
-		Spec: v1.PodSpec{
-			Containers: []v1.Container{
-				{
-					Name:            "sd-exporter",
-					Image:           "gcr.io/google-containers/sd-dummy-exporter:v0.1.0",
-					ImagePullPolicy: v1.PullPolicy("Always"),
-					Command:         []string{"/sd_dummy_exporter", "--pod-id=$(POD_ID)", "--metric-name=" + CustomMetricName, fmt.Sprintf("--metric-value=%v", MetricValue1)},
-					Env: []v1.EnvVar{
-						{
-							Name: "POD_ID",
-							ValueFrom: &v1.EnvVarSource{
-								FieldRef: &v1.ObjectFieldSelector{
-									FieldPath: "metadata.uid",
-								},
-							},
-						},
-					},
-					Ports: []v1.ContainerPort{{ContainerPort: 80}},
-				},
-			},
-		},
-	}
-	SDExporterPod2 = &v1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "sd-exporter-2",
-			Namespace: "default",
-			Labels: map[string]string{
-				"name": "sd-exporter",
-			},
-		},
-		Spec: v1.PodSpec{
-			Containers: []v1.Container{
-				{
-					Name:            "sd-exporter",
-					Image:           "gcr.io/google-containers/sd-dummy-exporter:v0.1.0",
-					ImagePullPolicy: v1.PullPolicy("Always"),
-					Command:         []string{"/sd_dummy_exporter", "--pod-id=$(POD_ID)", "--metric-name=" + CustomMetricName, fmt.Sprintf("--metric-value=%v", MetricValue2)},
-					Env: []v1.EnvVar{
-						{
-							Name: "POD_ID",
-							ValueFrom: &v1.EnvVarSource{
-								FieldRef: &v1.ObjectFieldSelector{
-									FieldPath: "metadata.uid",
-								},
-							},
-						},
-					},
-					Ports: []v1.ContainerPort{{ContainerPort: 80}},
-				},
-			},
-		},
-	}
 )
+
+func SDExporterDeployment(name string, replicas int32, metricValue int64) *extensions.Deployment {
+	return &extensions.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: "default",
+		},
+		Spec: extensions.DeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{"name": name},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"name": name,
+					},
+				},
+				Spec: sdExporterPodSpec(CustomMetricName, metricValue),
+			},
+			Replicas: &replicas,
+		},
+	}
+}
+
+func sdExporterPodSpec(metricName string, metricValue int64) corev1.PodSpec {
+	return corev1.PodSpec{
+		Containers: []corev1.Container{
+			{
+				Name:            "sd-exporter",
+				Image:           "gcr.io/google-containers/sd-dummy-exporter:v0.1.0",
+				ImagePullPolicy: corev1.PullPolicy("Always"),
+				Command:         []string{"/sd_dummy_exporter", "--pod-id=$(POD_ID)", "--metric-name=" + metricName, fmt.Sprintf("--metric-value=%v", metricValue)},
+				Env: []corev1.EnvVar{
+					{
+						Name: "POD_ID",
+						ValueFrom: &corev1.EnvVarSource{
+							FieldRef: &corev1.ObjectFieldSelector{
+								FieldPath: "metadata.uid",
+							},
+						},
+					},
+				},
+				Ports: []corev1.ContainerPort{{ContainerPort: 80}},
+			},
+		},
+	}
+}
+
+func SDExporterPod(podName, podLabel, metricName string, metricValue int64) *corev1.Pod {
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      podName,
+			Namespace: "default",
+			Labels: map[string]string{
+				"name": podLabel,
+			},
+		},
+		Spec: sdExporterPodSpec(metricName, metricValue),
+	}
+}
