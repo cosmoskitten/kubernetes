9243db2f512f6b89e07c5b3bdd633c236cb41e55
Marian Lobur <loburm@google.com>
2017-09-27 09:53:41 +0200

    Improve e2e tests of audit logging.
    Now test includes:
     * Verbs: create, list, watch, delete, get.
     * Resources: pods, deployments, secrets, config maps, custom resource
     definition.
     * More fields: user, resource, level, stage, presence of request and
     response objects.

--- test/e2e/auth/audit.go
+++ test/e2e/auth/audit.go
@@ -21,14 +21,25 @@ import (
 	"encoding/json"
 	"fmt"
 	"strings"
+	"time"
 
 	apiv1 "k8s.io/api/core/v1"
+	extensions "k8s.io/api/extensions/v1beta1"
+	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	"k8s.io/apiextensions-apiserver/test/integration/testserver"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apiserver/pkg/apis/audit/v1beta1"
 	"k8s.io/kubernetes/test/e2e/framework"
+	imageutils "k8s.io/kubernetes/test/utils/image"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+)
+
+const (
+	watchTimeout = 1
+	user         = "kubecfg"
 )
 
 var _ = SIGDescribe("Advanced Audit [Feature:Audit]", func() {
@@ -37,7 +48,6 @@ var _ = SIGDescribe("Advanced Audit [Feature:Audit]", func() {
 	It("should audit API calls", func() {
 		namespace := f.Namespace.Name
 
-		// Create & Delete pod
 		pod := &apiv1.Pod{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "audit-pod",
@@ -49,10 +59,10 @@ var _ = SIGDescribe("Advanced Audit [Feature:Audit]", func() {
 				}},
 			},
 		}
-		f.PodClient().CreateSync(pod)
-		f.PodClient().DeleteSync(pod.Name, &metav1.DeleteOptions{}, framework.DefaultPodDeletionTimeout)
 
-		// Create, Read, Delete secret
+		podLabels := map[string]string{"name": "audit-deployment-pod"}
+		d := framework.NewDeployment("audit-deployment", int32(1), podLabels, "redis", imageutils.GetE2EImage(imageutils.Redis), extensions.RecreateDeploymentStrategyType)
+
 		secret := &apiv1.Secret{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "audit-secret",
@@ -61,45 +71,123 @@ var _ = SIGDescribe("Advanced Audit [Feature:Audit]", func() {
 				"top-secret": []byte("foo-bar"),
 			},
 		}
-		_, err := f.ClientSet.Core().Secrets(f.Namespace.Name).Create(secret)
+
+		configMap := &apiv1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "audit-configmap",
+			},
+			Data: map[string]string{
+				"map-key": "map-value",
+			},
+		}
+
+		watchOptions := metav1.ListOptions{TimeoutSeconds: intToPointer(watchTimeout)}
+
+		f.PodClient().CreateSync(pod)
+		_, err := f.PodClient().Get(pod.Name, metav1.GetOptions{})
+		framework.ExpectNoError(err, "failed to get audit-pod")
+		_, err = f.PodClient().Watch(watchOptions)
+		framework.ExpectNoError(err, "failed to create watch for pods")
+		_, err = f.PodClient().List(metav1.ListOptions{})
+		framework.ExpectNoError(err, "failed to list pods")
+		f.PodClient().DeleteSync(pod.Name, &metav1.DeleteOptions{}, framework.DefaultPodDeletionTimeout)
+
+		_, err = f.ClientSet.Extensions().Deployments(f.Namespace.Name).Create(d)
+		framework.ExpectNoError(err, "failed to create audit-deployment")
+		_, err = f.ClientSet.Extensions().Deployments(f.Namespace.Name).Get(d.Name, metav1.GetOptions{})
+		framework.ExpectNoError(err, "failed to get audit-deployment")
+		_, err = f.ClientSet.Extensions().Deployments(f.Namespace.Name).Watch(watchOptions)
+		framework.ExpectNoError(err, "failed to create watch for deployments")
+		_, err = f.ClientSet.Extensions().Deployments(f.Namespace.Name).List(metav1.ListOptions{})
+		framework.ExpectNoError(err, "failed to create list deployments")
+		err = f.ClientSet.Extensions().Deployments(f.Namespace.Name).Delete("audit-deployment", &metav1.DeleteOptions{})
+		framework.ExpectNoError(err, "failed to delete deployments")
+
+		_, err = f.ClientSet.Core().Secrets(f.Namespace.Name).Create(secret)
 		framework.ExpectNoError(err, "failed to create audit-secret")
 		_, err = f.ClientSet.Core().Secrets(f.Namespace.Name).Get(secret.Name, metav1.GetOptions{})
 		framework.ExpectNoError(err, "failed to get audit-secret")
+		_, err = f.ClientSet.Core().Secrets(f.Namespace.Name).List(metav1.ListOptions{})
+		framework.ExpectNoError(err, "failed to list secrets")
+		_, err = f.ClientSet.Core().Secrets(f.Namespace.Name).Watch(watchOptions)
+		framework.ExpectNoError(err, "failed to create watch for secrets")
 		err = f.ClientSet.Core().Secrets(f.Namespace.Name).Delete(secret.Name, &metav1.DeleteOptions{})
 		framework.ExpectNoError(err, "failed to delete audit-secret")
 
-		expectedEvents := []auditEvent{{
-			method:    "create",
-			namespace: namespace,
-			uri:       fmt.Sprintf("/api/v1/namespaces/%s/pods", namespace),
-			response:  "201",
-		}, {
-			method:    "delete",
-			namespace: namespace,
-			uri:       fmt.Sprintf("/api/v1/namespaces/%s/pods/%s", namespace, pod.Name),
-			response:  "200",
-		}, {
-			method:    "create",
-			namespace: namespace,
-			uri:       fmt.Sprintf("/api/v1/namespaces/%s/secrets", namespace),
-			response:  "201",
-		}, {
-			method:    "get",
-			namespace: namespace,
-			uri:       fmt.Sprintf("/api/v1/namespaces/%s/secrets/%s", namespace, secret.Name),
-			response:  "200",
-		}, {
-			method:    "delete",
-			namespace: namespace,
-			uri:       fmt.Sprintf("/api/v1/namespaces/%s/secrets/%s", namespace, secret.Name),
-			response:  "200",
-		}}
+		_, err = f.ClientSet.Core().ConfigMaps(f.Namespace.Name).Create(configMap)
+		framework.ExpectNoError(err, "failed to create audit-configmap")
+		_, err = f.ClientSet.Core().ConfigMaps(f.Namespace.Name).Get(configMap.Name, metav1.GetOptions{})
+		framework.ExpectNoError(err, "failed to get audit-configmap")
+		_, err = f.ClientSet.Core().ConfigMaps(f.Namespace.Name).List(metav1.ListOptions{})
+		framework.ExpectNoError(err, "failed to list config maps")
+		_, err = f.ClientSet.Core().ConfigMaps(f.Namespace.Name).Watch(watchOptions)
+		framework.ExpectNoError(err, "failed to create watch for config maps")
+		err = f.ClientSet.Core().ConfigMaps(f.Namespace.Name).Delete(configMap.Name, &metav1.DeleteOptions{})
+		framework.ExpectNoError(err, "failed to delete audit-configmap")
+
+		config, err := framework.LoadConfig()
+		if err != nil {
+			framework.Failf("failed to load config: %v", err)
+		}
+		apiExtensionClient, err := clientset.NewForConfig(config)
+		if err != nil {
+			framework.Failf("failed to initialize apiExtensionClient: %v", err)
+		}
+		crd := testserver.NewRandomNameCustomResourceDefinition(apiextensionsv1beta1.ClusterScoped)
+		_, err = testserver.CreateNewCustomResourceDefinition(crd, apiExtensionClient, f.ClientPool)
+		testserver.DeleteCustomResourceDefinition(crd, apiExtensionClient)
+
+		expectedEvents := []auditEvent{}
+		expectedEvents = append(expectedEvents, commonExpectedEvents("pods", namespace, "create", pod.Name)...)
+		expectedEvents = append(expectedEvents, commonExpectedEvents("pods", namespace, "delete", pod.Name)...)
+		expectedEvents = append(expectedEvents, commonExpectedEvents("pods", namespace, "get", pod.Name)...)
+		expectedEvents = append(expectedEvents, commonExpectedEvents("pods", namespace, "list", pod.Name)...)
+
+		expectedEvents = append(expectedEvents, commonExpectedEvents("deployments", namespace, "create", d.Name)...)
+		expectedEvents = append(expectedEvents, commonExpectedEvents("deployments", namespace, "delete", d.Name)...)
+		expectedEvents = append(expectedEvents, commonExpectedEvents("deployments", namespace, "get", d.Name)...)
+		expectedEvents = append(expectedEvents, commonExpectedEvents("deployments", namespace, "list", d.Name)...)
+
+		expectedEvents = append(expectedEvents, commonExpectedEvents("secrets", namespace, "create", secret.Name)...)
+		expectedEvents = append(expectedEvents, commonExpectedEvents("secrets", namespace, "delete", secret.Name)...)
+		expectedEvents = append(expectedEvents, commonExpectedEvents("secrets", namespace, "get", secret.Name)...)
+		expectedEvents = append(expectedEvents, commonExpectedEvents("secrets", namespace, "list", secret.Name)...)
+
+		expectedEvents = append(expectedEvents, commonExpectedEvents("configmaps", namespace, "create", configMap.Name)...)
+		expectedEvents = append(expectedEvents, commonExpectedEvents("configmaps", namespace, "delete", configMap.Name)...)
+		expectedEvents = append(expectedEvents, commonExpectedEvents("configmaps", namespace, "get", configMap.Name)...)
+		expectedEvents = append(expectedEvents, commonExpectedEvents("configmaps", namespace, "list", configMap.Name)...)
+
+		expectedEvents = append(expectedEvents, watchExpectedEvents("pods", namespace)...)
+		expectedEvents = append(expectedEvents, watchExpectedEvents("deployments", namespace)...)
+		expectedEvents = append(expectedEvents, watchExpectedEvents("secrets", namespace)...)
+		expectedEvents = append(expectedEvents, watchExpectedEvents("configmaps", namespace)...)
+
+		expectedEvents = append(expectedEvents, customResourceEvents("create", *crd)...)
+		expectedEvents = append(expectedEvents, customResourceEvents("delete", *crd)...)
+
+		// Sleep for watches to timeout.
+		time.Sleep(3 * watchTimeout * time.Second)
+
 		expectAuditLines(f, expectedEvents)
 	})
 })
 
+func intToPointer(num int64) *int64 {
+	return &num
+}
+
 type auditEvent struct {
-	method, namespace, uri, response string
+	level          v1beta1.Level
+	stage          v1beta1.Stage
+	requestURI     string
+	verb           string
+	code           int32
+	user           string
+	resource       string
+	namespace      string
+	requestObject  bool
+	responseObject bool
 }
 
 // Search the audit log for the expected audit lines.
@@ -134,46 +222,142 @@ func expectAuditLines(f *framework.Framework, expected []auditEvent) {
 
 func parseAuditLine(line string) (auditEvent, error) {
 	var e v1beta1.Event
-	if err := json.Unmarshal([]byte(line), &e); err == nil {
-		event := auditEvent{
-			method: e.Verb,
-			uri:    e.RequestURI,
-		}
-		if e.ObjectRef != nil {
-			event.namespace = e.ObjectRef.Namespace
-		}
-		if e.ResponseStatus != nil {
-			event.response = fmt.Sprintf("%d", e.ResponseStatus.Code)
-		}
-		return event, nil
+	if err := json.Unmarshal([]byte(line), &e); err != nil {
+		return auditEvent{}, err
 	}
-
-	fields := strings.Fields(line)
-	if len(fields) < 3 {
-		return auditEvent{}, fmt.Errorf("could not parse audit line: %s", line)
+	event := auditEvent{
+		level:      e.Level,
+		stage:      e.Stage,
+		requestURI: e.RequestURI,
+		verb:       e.Verb,
+		user:       e.User.Username,
 	}
-	// Ignore first field (timestamp)
-	if fields[1] != "AUDIT:" {
-		return auditEvent{}, fmt.Errorf("unexpected audit line format: %s", line)
+	if e.ObjectRef != nil {
+		event.namespace = e.ObjectRef.Namespace
+		event.resource = e.ObjectRef.Resource
 	}
-	fields = fields[2:]
-	event := auditEvent{}
-	for _, f := range fields {
-		parts := strings.SplitN(f, "=", 2)
-		if len(parts) != 2 {
-			return auditEvent{}, fmt.Errorf("could not parse audit line (part: %q): %s", f, line)
-		}
-		value := strings.Trim(parts[1], "\"")
-		switch parts[0] {
-		case "method":
-			event.method = value
-		case "namespace":
-			event.namespace = value
-		case "uri":
-			event.uri = value
-		case "response":
-			event.response = value
-		}
+	if e.ResponseStatus != nil {
+		event.code = e.ResponseStatus.Code
+	}
+	if e.ResponseObject != nil {
+		event.responseObject = true
+	}
+	if e.RequestObject != nil {
+		event.requestObject = true
 	}
 	return event, nil
+}
+
+func customResourceEvents(method string, crd apiextensionsv1beta1.CustomResourceDefinition) []auditEvent {
+	ar := strings.SplitN(crd.Name, ".", 2)
+	name := ar[0]
+	namespace := ar[1]
+	apiEvent := auditEvent{
+		level:          v1beta1.LevelRequestResponse,
+		stage:          v1beta1.StageResponseComplete,
+		requestURI:     "/apis/apiextensions.k8s.io/v1beta1/customresourcedefinitions",
+		verb:           method,
+		code:           code(method),
+		user:           user,
+		resource:       "customresourcedefinitions",
+		requestObject:  true,
+		responseObject: true,
+	}
+	objectEvent := auditEvent{
+		level:          v1beta1.LevelMetadata,
+		stage:          v1beta1.StageResponseComplete,
+		requestURI:     fmt.Sprintf("/apis/%s/v1beta1/%s", namespace, name),
+		verb:           method,
+		code:           code(method),
+		user:           user,
+		resource:       name,
+		requestObject:  false,
+		responseObject: false,
+	}
+	if method == "delete" {
+		apiEvent.requestObject = false
+		apiEvent.requestURI = fmt.Sprintf("/apis/apiextensions.k8s.io/v1beta1/customresourcedefinitions/%s", crd.Name)
+		objectEvent.requestURI = fmt.Sprintf("/apis/%s/v1beta1/%s/setup-instance", namespace, name)
+	}
+	return []auditEvent{apiEvent, objectEvent}
+}
+
+func commonExpectedEvents(resource string, namespace string, method string, resourceName string) []auditEvent {
+	return []auditEvent{
+		{
+			level:          level(resource, method),
+			stage:          v1beta1.StageResponseComplete,
+			requestURI:     uriString(resource, namespace, method, resourceName),
+			verb:           method,
+			code:           code(method),
+			user:           user,
+			resource:       resource,
+			namespace:      namespace,
+			requestObject:  containsRequestBody(resource, method),
+			responseObject: containsRequestBody(resource, method),
+		},
+	}
+}
+
+func containsRequestBody(resource string, method string) bool {
+	if (resource == "pods" || resource == "deployments") && (method == "create" || method == "delete") {
+		return true
+	} else {
+		return false
+	}
+}
+
+func uriString(resource string, namespace string, method string, resourceName string) string {
+	if method == "create" || method == "list" {
+		return fmt.Sprintf("%s/namespaces/%s/%s", api(resource), namespace, resource)
+	} else {
+		return fmt.Sprintf("%s/namespaces/%s/%s/%s", api(resource), namespace, resource, resourceName)
+	}
+}
+
+func code(method string) int32 {
+	if method == "create" {
+		return 201
+	} else {
+		return 200
+	}
+}
+
+func level(resource string, method string) v1beta1.Level {
+	if resource == "secrets" || resource == "configmaps" {
+		return v1beta1.LevelMetadata
+	} else if method == "delete" || method == "create" {
+		return v1beta1.LevelRequestResponse
+	} else {
+		return v1beta1.LevelRequest
+	}
+}
+
+func api(resource string) string {
+	if resource == "deployments" {
+		return "/apis/extensions/v1beta1"
+	} else {
+		return "/api/v1"
+	}
+}
+
+func watchExpectedEvents(resource string, namespace string) []auditEvent {
+	responseStarted := auditEvent{
+		level:          level(resource, ""),
+		stage:          v1beta1.StageResponseStarted,
+		requestURI:     fmt.Sprintf("%s/namespaces/%s/%s?timeoutSeconds=%d&watch=true", api(resource), namespace, resource, watchTimeout),
+		verb:           "watch",
+		code:           200,
+		user:           user,
+		resource:       resource,
+		namespace:      namespace,
+		requestObject:  false,
+		responseObject: false,
+	}
+	responseComplete := responseStarted
+	responseComplete.stage = v1beta1.StageResponseComplete
+	return []auditEvent{
+		responseStarted,
+		responseComplete,
+	}
 }
