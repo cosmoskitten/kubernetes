5c381d74c45ca5f896baa172954b7c611c2045da
Zihong Zheng <zihongz@google.com>
2017-10-05 11:10:22 -0700

    [REMOVE ME] tweak KUBE_PROXY_DAEMONSET for testing

--- cluster/gce/config-test.sh
+++ cluster/gce/config-test.sh
@@ -376,7 +376,7 @@ ENABLE_PROMETHEUS_TO_SD="${ENABLE_PROMETHEUS_TO_SD:-true}"
 
 # TODO(#51292): Make kube-proxy Daemonset default and remove the configuration here.
 # Optional: [Experiment Only] Run kube-proxy as a DaemonSet if set to true, run as static pods otherwise.
-KUBE_PROXY_DAEMONSET="${KUBE_PROXY_DAEMONSET:-false}" # true, false
+KUBE_PROXY_DAEMONSET="${KUBE_PROXY_DAEMONSET:-true}" # true, false
 
 # Optional: duration of cluster signed certificates.
 CLUSTER_SIGNING_DURATION="${CLUSTER_SIGNING_DURATION:-}"
