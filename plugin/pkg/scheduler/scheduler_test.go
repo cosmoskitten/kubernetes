27496f0f70abd7352579245c214922e249734efa
Gavin <li.guangxu@zte.com.cn>
2017-10-06 21:58:59 +0800

    clarify pridicates message when no nodes available
    fix space

--- plugin/pkg/scheduler/scheduler_test.go
+++ plugin/pkg/scheduler/scheduler_test.go
@@ -300,6 +300,7 @@ func TestSchedulerNoPhantomPodAfterDelete(t *testing.T) {
 	case err := <-errChan:
 		expectErr := &core.FitError{
 			Pod:              secondPod,
+			TotalNodesNum:    1,
 			FailedPredicates: core.FailedPredicateMap{node.Name: []algorithm.PredicateFailureReason{predicates.ErrPodNotFitsHostPorts}},
 		}
 		if !reflect.DeepEqual(expectErr, err) {
@@ -484,6 +485,7 @@ func TestSchedulerFailedSchedulingReasons(t *testing.T) {
 	case err := <-errChan:
 		expectErr := &core.FitError{
 			Pod:              podWithTooBigResourceRequests,
+			TotalNodesNum:    len(nodes),
 			FailedPredicates: failedPredicatesMap,
 		}
 		if len(fmt.Sprint(expectErr)) > 150 {
