b6481197c0e19dd49773270e29b3cfcf927265d8
David Eads <deads@redhat.com>
2017-10-11 10:06:37 -0400

    allow */subresource in rbac policy rules

--- pkg/apis/rbac/helpers.go
+++ pkg/apis/rbac/helpers.go
@@ -55,12 +55,28 @@ func APIGroupMatches(rule *PolicyRule, requestedGroup string) bool {
 	return false
 }
 
-func ResourceMatches(rule *PolicyRule, requestedResource string) bool {
+func ResourceMatches(rule *PolicyRule, combinedRequestedResource, requestedSubresource string) bool {
 	for _, ruleResource := range rule.Resources {
+		// if everything is allowed, we match
 		if ruleResource == ResourceAll {
 			return true
 		}
-		if ruleResource == requestedResource {
+		// if we have an exact match, we match
+		if ruleResource == combinedRequestedResource {
+			return true
+		}
+
+		// We can also match a */subresource.
+		// if there isn't a subresource, then continue
+		if len(requestedSubresource) == 0 {
+			continue
+		}
+		ruleTokens := strings.SplitN(ruleResource, "/", 2)
+		// if the rule isn't in the format */subresource, then we don't match, continue
+		if len(ruleTokens) != 2 || ruleTokens[0] != ResourceAll {
+			continue
+		}
+		if ruleTokens[1] == requestedSubresource {
 			return true
 		}
 	}
